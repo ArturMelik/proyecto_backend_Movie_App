a9dfa872ded0e551c8463911c650cbb3fe9c378a
Artur <divermsurartur@gmail.com>
2025-11-20 12:42:33 +0100

    add/scraps

--- utils/scraper.js
+++ utils/scraper.js
@@ -11,10 +11,35 @@ const extractMovieData = async (url, browser) => {
         // Aquí irán los selectors que tú encuentres para:
         // media de reseña y opiniones
 
+        //Titulo pelicula
+        movieData['title'] = await page.$eval(".meta-title-link", title => title.innerHTML);
+
+        //Year
+        movieData['year'] = await page.$eval(".date", year => year.innerHTML);
+
+        //Genre
+        movieData['genre'] = await page.$eval(".dark-grey-link", genre => genre.innerHTML);
+        
+        //Director
+        movieData['director'] = await page.$$eval('.meta-body-item.meta-body-direction a',dirs => dirs.map(d => d.innerText.trim()));
+
+        //Actors
+        movieData['actors'] = await page.$$eval('.meta-body-actor a', links => links.map(a => a.innerText.trim()));
+
+        //Plot
+        let plot = await page.$eval(".content-txt", el => el.innerText.trim());
+
+        movieData['plot'] = plot.length > 200
+            ? plot.substring(0, 200) + "..."
+            : plot;
+
+
+        //Rating
         movieData['rating'] = await page.$eval(".stareval-note", rating => rating.innerHTML);
         
-        // opinions: suponiendo que hay varios elementos con opiniones
+        //Opinions
         movieData['reviews'] = await page.$$eval(".editorial-content.cf", reviews => reviews.map(r => r.innerText).slice(0, 5));
+
 
         return movieData;
     } catch (err) {
